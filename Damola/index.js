63e5d7f27171a400427fb60c295827c3008dc406
DamolaAdewunmi <damolasd@gmail.com>
2019-05-23 14:46:56 +0100

    About page 80% done, Landing page completed

--- Damola/index.js
+++
@@ -1,5 +0,0 @@
-// function OpenAbout() { // Target refers to the clicked element
-//     location.href='about-us.html';
-// };
-const aboutButton = document.querySelector('.about-page');
-aboutButton.style.color = "red";
